29d80f48d9091caed8b97c9d09e9bb37216b89e5
Patrick von Massow | GIP <patrick.vonmassow@gip.com>
2023-09-20 15:37:47 +0200

    re-added copyright header and moved data type from project to product

--- events/message-bus.service.ts
+++ events/message-bus.service.ts
@@ -1,7 +1,23 @@
+/*
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Copyright 2023 Xyna GmbH, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { XoMessage } from '@project/geko/xo/xmcp/yggdrasil/message.model';
 import { FullQualifiedName, RuntimeContext } from '@zeta/api/xo/xo-describer';
 import { AuthService } from '@zeta/auth';
 import { randomUUID } from '@zeta/base';
@@ -16,6 +32,7 @@ import { XoDocumentRelationsChange } from './xo/document-relations-change.model'
 import { XoDocumentUnlock } from './xo/document-unlock.model';
 import { XoEvent } from './xo/event.model';
 import { XoGetEventsResponse } from './xo/get-events-response.model';
+import { XoMessage } from './xo/message.model';
 import { XoOISChange } from './xo/ois-change.model';
 import { XoProjectEvent } from './xo/project-event.model';
 import { XoRemoteDestinationsChange } from './xo/remote-destinations-change.model';
