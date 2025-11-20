e93ee056408f6c77723a4d1a07e85aa4cb400b23
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2025-11-20 09:55:03 +0100

    update version

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const YggdrasilName = 'Yggdrasil';
-export const YggdrasilVersion = '1.6.2';
+export const YggdrasilVersion = '1.6.1';
